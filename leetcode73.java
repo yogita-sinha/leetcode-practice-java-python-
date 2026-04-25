5ee8e280697560454ed219aa388b1a7f4e729530
yogita-sinha <yogita.sinha2606@gmail.com>
2026-04-25 12:57:45 +0530

    Add files via upload

---
+++ leetcode73.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+public class leetcode73 {
+
+    public static void main(String[] args) {
+
+        int[][] matrix = {
+            {1, 1, 1},
+            {1, 0, 1},
+            {1, 1, 1}
+        };
+
+        setZeroes(matrix);
+
+        // print result
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[0].length; j++){
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void setZeroes(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        boolean[] row = new boolean[n];
+        boolean[] col = new boolean[m];
+
+        // mark
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(matrix[i][j] == 0){
+                    row[i] = true;
+                    col[j] = true;
+                }
+            }
+        }
+
+        // set zeroes
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(row[i] || col[j]){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
